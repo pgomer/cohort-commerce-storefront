fbe61b90ae1d541c9595d34a1d3b9bb150188240
Phil Gomer <pgomer@adobe.com>
2026-09-24 14:44:35 -0400

    bug fixes

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -152,6 +152,26 @@ export default async function decorate(block) {
 
   block.replaceChildren(fragment);
 
+  events.on('pdp/data', (callbackProduct) => {
+    if (!callbackProduct) return;
+    if (callbackProduct.inStock) {
+      $stock.textContent = '● In Stock';
+      $stock.className = 'product-details__stock stock-badge stock-badge--in-stock';
+    } else {
+      $stock.textContent = '● Out of Stock';
+      $stock.className = 'product-details__stock stock-badge stock-badge--out-of-stock';
+    }
+    const value = callbackProduct.metaTitle;
+    if (value) {
+      $customAttribute.innerHTML = `
+      <div class="custom-attribute">
+      <dt>Custom Meta Title - Activity 2.1</dt>
+      <dd>${value}</dd>
+      </div>
+      `;
+    }
+  }, { eager: true });
+
   if ($tagline) {
     $tagline.textContent = 'Free shipping on orders over $50';
   }
@@ -434,26 +454,6 @@ export default async function decorate(block) {
 
   return Promise.resolve();
 }
-
-events.on('pdp/data', (product) => {
-  if (!product) return;
-  if (product.inStock) {
-    $stock.textContent = '● In Stock';
-    $stock.className = 'product-details__stock stock-badge stock-badge--in-stock';
-  } else {
-    $stock.textContent = '● Out of Stock';
-    $stock.className = 'product-details__stock stock-badge stock-badge--out-of-stock';
-  }
-  const value = product.metaTitle;
-  if (value) {
-    $customAttribute.innerHTML = `
-    <div class="custom-attribute">
-    <dt>Custom Meta Title - Activity 2.1</dt>
-    <dd>${value}</dd>
-    </div>
-    `;
-  }
-}, { eager: true });
 
 async function setJsonLdProduct(product) {
   const {
