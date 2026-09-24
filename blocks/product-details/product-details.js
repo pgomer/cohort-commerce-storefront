53392dad255bc544434795f9da4182cc18621a3a
Phil Gomer <pgomer@adobe.com>
2026-09-24 14:18:42 -0400

    bug fixes

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -151,27 +151,26 @@ export default async function decorate(block) {
   const $customAttribute = fragment.querySelector('.product-details__custom-attribute');
 
   block.replaceChildren(fragment);
-
   
-events.on('pdp/data', (product) => {
-    if (!product) return;
-    if (product.inStock) {
-      $stock.textContent = '● In Stock';
-      $stock.className = 'product-details__stock stock-badge stock-badge--in-stock';
-    } else {
-      $stock.textContent = '● Out of Stock';
-      $stock.className = 'product-details__stock stock-badge stock-badge--out-of-stock';
-    }
-    const value = product.metaTitle;
-    if (value) {
-      $customAttribute.innerHTML = `
-      <div class="custom-attribute">
-      <dt>Custom Meta Title - Activity 2.1</dt>
-      <dd>${value}</dd>
-      </div>
-      `;
-    }
-  }, { eager: true });
+  events.on('pdp/data', (product) => {
+      if (!product) return;
+      if (product.inStock) {
+        $stock.textContent = '● In Stock';
+        $stock.className = 'product-details__stock stock-badge stock-badge--in-stock';
+      } else {
+        $stock.textContent = '● Out of Stock';
+        $stock.className = 'product-details__stock stock-badge stock-badge--out-of-stock';
+      }
+      const value = product.metaTitle;
+      if (value) {
+        $customAttribute.innerHTML = `
+        <div class="custom-attribute">
+        <dt>Custom Meta Title - Activity 2.1</dt>
+        <dd>${value}</dd>
+        </div>
+        `;
+      }
+    }, { eager: true });
 
   if ($tagline) {
     $tagline.textContent = 'Free shipping on orders over $50';
